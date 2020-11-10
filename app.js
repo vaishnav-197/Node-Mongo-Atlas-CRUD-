6fc89cdd2d1f6e8da735e1ec6d90577db36a6824
vaishnav-197 <vaishnavea18@gmail.com>
2020-11-11 01:22:08 +0530

    added basic form validation

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ app.use(expressLayouts);
 app.set('view engine' , 'ejs')
 
 
+// bodyparser
+app.use(express.urlencoded({extended: false}));
+
 // Routes
 app.use('/' , require('./routes/index'));
 app.use('/users' , require('./routes/users'));
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,40 @@ router.get('/login' , (req , res) =>  res.render('login'))
 // login
 router.get('/register' , (req , res) =>  res.render('register'))
 
+// Register
+router.post('/register' , (req,res) => {
+    const { name , email , password , password2} = req.body;
+    let errors = [];
+
+    
+    // req fields
+    if(!name || !email || !password || !password2){
+        errors.push({ msg:' Please fill in all the fields'});
+    }
+    
+    // confirm passwords
+    if( password != password2){
+        errors.push({ msg:' passwords doesnt match'});
+    }
+    
+    // check linegth
+    if(password.length < 6){
+        errors.push({ msg:' Atleast 6 characters'});
+    }
+    if(errors.length > 0){
+        res.render('register' , {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+        
+    }
+    else{
+        res.send('pass');
+    }
+});
 
 
 module.exports = router;
